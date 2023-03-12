ddf3691fca78bee33eeeb5286d2cde2b89fc1b79
andre-ortu <andrea.ortu.9229@gmail.com>
2023-03-12 21:42:29 +0100

    chore: Add GymCard Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\GymCard;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,9 +16,33 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+         $user = \App\Models\User::create([
+             'name' => 'Andrea',
+             'surname' => 'Ortu',
+             'email' => 'andrea.ortu.9229@gmail.com',
+             'password' => Hash::make('password')
+         ]);
+
+         GymCard::create([
+             'user_id' => $user->id,
+             'day' => 1,
+             'exercise' => 1,
+             'name' => 'Cyclette',
+             'reps' => '4/5min',
+         ]);
+        GymCard::create([
+            'user_id' => $user->id,
+            'day' => 1,
+            'exercise' => 2,
+            'name' => 'Panca Piana',
+            'reps' => '1m30sec',
+        ]);
+        GymCard::create([
+            'user_id' => $user->id,
+            'day' => 1,
+            'exercise' => 2,
+            'name' => 'Panca Inclinata',
+            'reps' => '1m30sec',
+        ]);
     }
 }
